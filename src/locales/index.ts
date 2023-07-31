df18df99f49d8d83b7137c84db4061288ee2360d
Daniel <github.danimoh@neverbox.com>
2023-07-31 13:37:31 +0200

    Internationalization: split translations for api constants and providers into separate files
    I.e. split translations that don't use the English source string as translation key into
    separate files. This is in preparation for switching to gettext PO translation files and
    extracting the source strings directly from the source code via our vue-i18n gettext
    extractor.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,29 @@
 import en from './en.json';
+import apiConstantsEn from './api-constants-en.json';
+import providersEn from './providers-en.json';
+
 import de from './de.json';
+import apiConstantsDe from './api-constants-de.json';
+import providersDe from './providers-de.json';
+
 import es from './es.json';
+import apiConstantsEs from './api-constants-es.json';
+import providersEs from './providers-es.json';
 
 export const messages = {
-  en,
-  de,
-  es,
+  en: {
+    ...en,
+    ...apiConstantsEn,
+    providers: providersEn,
+  },
+  de: {
+    ...de,
+    ...apiConstantsDe,
+    providers: providersDe,
+  },
+  es: {
+    ...es,
+    ...apiConstantsEs,
+    providers: providersEs,
+  },
 }
