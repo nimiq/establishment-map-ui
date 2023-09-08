8a91a5c415160834d3aa2ea6dda9e6d2c74a55de
maximo <maximo@nimiq.com>
2023-09-08 13:43:24 +0700

    handle antimeridians with turf

--- shared/geo-utils.ts
+++ shared/geo-utils.ts
@@ -1,6 +1,7 @@
 import bboxPolygon from '@turf/bbox-polygon'
-import { type MultiPolygon, featureCollection, multiPolygon, point } from '@turf/helpers'
 import booleanWithin from '@turf/boolean-within'
+import type { MultiPolygon } from '@turf/helpers'
+import { featureCollection, multiPolygon, point } from '@turf/helpers'
 import pointsWithinPolygon from '@turf/points-within-polygon'
 import union from '@turf/union'
 import type { BoundingBox, Point } from '../types/index.ts'
@@ -13,8 +14,14 @@ export const toPoint = <T extends Point>(data: T) => point([data.lng, data.lat],
 
 /**
  * Converts a bounding box to a GeoJSON Polygon
+ *
+ * It creates an array of polygons. This is just in case the bounding box crosses the antimeridian,
+ * which will result in two polygons.
  */
-export const toPolygon = ({ swLat, neLat, neLng, swLng }: BoundingBox) => bboxPolygon([swLng, swLat, neLng, neLat])
+const _toPolygon = ({ swLat, neLat, neLng, swLng }: BoundingBox) => bboxPolygon([swLng, swLat, neLng, neLat])
+function toPolygon(bbox: BoundingBox) {
+  return bbox.swLng > bbox.neLng ? [_toPolygon({ ...bbox, neLng: 180 }), _toPolygon({ ...bbox, swLng: -180 })] : [_toPolygon(bbox)]
+}
 
 /**
  * A mutlipolygon is a list of polygons.
@@ -31,30 +38,31 @@ export const toPolygon = ({ swLat, neLat, neLng, swLng }: BoundingBox) => bboxPo
 /**
  * Converts a bounding box to a GeoJSON MultiPolygon
  */
-export const toMultiPolygon = (bbox: BoundingBox) => multiPolygon([toPolygon(bbox).geometry.coordinates])
+export const toMultiPolygon = (bbox: BoundingBox) => multiPolygon(toPolygon(bbox).map((p => p.geometry.coordinates)))
 
 /**
  * Checks if a bounding box is within a multipolygon
  */
-export const bBoxIsWithinArea = (bbox: BoundingBox, multiPoly?: MultiPolygon) => !multiPoly ? false : booleanWithin(toPolygon(bbox), multiPoly)
+export const bBoxIsWithinArea = (bbox: BoundingBox, multiPoly?: MultiPolygon) => !multiPoly ? false : booleanWithin(toMultiPolygon(bbox), multiPoly)
 
 /**
  * Adds a polygon (from a bounding box) to a multipolygon
  */
 export function addBBoxToArea(bbox: BoundingBox, multiPoly?: MultiPolygon) {
-  return !multiPoly ? toMultiPolygon(bbox).geometry : union(multiPoly, toPolygon(bbox))?.geometry as MultiPolygon || multiPoly
+  return !multiPoly ? toMultiPolygon(bbox).geometry : union(multiPoly, toMultiPolygon(bbox))?.geometry as MultiPolygon || multiPoly
 }
 
 /**
  * Given a list of items (anything that has {lat, lng}, e.g.: locations or clusters) and a bounding box,
  * returns the items that are within the bounding box
  *
- * 1. We create a list of points (using multiPoint) from the items
+ * 1. Split the box if we are in the antimeridian
+ * 2. We create a list of points (using multiPoint) from the items
  *    - In the properties of each point, we store the items data
- * 2. We create a polygon from the bounding box
- * 3. We check which points are within the polygon
- * 4. We return the origina item data from the points that are within the polygon
+ * 3. We create a polygon from each of the bounding box from step 1
+ * 4. We check which points are within any of the polygons
+ * 5. We return the original item data from the points that are within the polygon
  */
 export function getItemsWithinBBox<T extends Point>(items: T[], bbox: BoundingBox) {
-  return pointsWithinPolygon(featureCollection(items.map(toPoint)), toPolygon(bbox)).features.flatMap(f => f.properties)
+  return pointsWithinPolygon(featureCollection(items.map(toPoint)), toMultiPolygon(bbox)).features.flatMap(f => f.properties)
 }
