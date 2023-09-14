82c539526a6e756d667dffbc0902204b6820ff3f
maximo <maximo@nimiq.com>
2023-09-14 10:42:41 +0700

    avoid requesting token if it already exists

--- database/functions.ts
+++ database/functions.ts
@@ -3,27 +3,26 @@ import type { InsertWithPlaceIdArgs } from '../types/database.ts'
 import { DbWriteFunction } from '../types/database.ts'
 import { fetchDb, getAuth } from './fetch.ts'
 
+async function insertTokenIntoAuth(dbArgs: DatabaseAuthArgs) {
+  return dbArgs.token ? { ...dbArgs } : { ...dbArgs, token: await getAuth(dbArgs) }
+}
+
 export async function addLocation(dbArgs: DatabaseAuthArgs, location: Omit<RawLocation, 'uuid'>) {
-  const token = await getAuth(dbArgs)
-  return await fetchDb<Location>(DbWriteFunction.UpsertRawLocation, { ...dbArgs, token }, JSON.stringify({ location }))
+  return await fetchDb<Location>(DbWriteFunction.UpsertRawLocation, await insertTokenIntoAuth(dbArgs), JSON.stringify({ location }))
 }
 
 export async function addLocationWithPlaceId(dbArgs: DatabaseAuthArgs, locations: InsertWithPlaceIdArgs[]): Promise<InsertWithPlaceIdResponse | undefined> {
-  const token = await getAuth(dbArgs)
-  return await fetchDb<InsertWithPlaceIdResponse>(DbWriteFunction.UpsertLocationsWithGMaps, { ...dbArgs, token }, JSON.stringify({ locations }))
+  return await fetchDb<InsertWithPlaceIdResponse>(DbWriteFunction.UpsertLocationsWithGMaps, await insertTokenIntoAuth(dbArgs), JSON.stringify({ locations }))
 }
 
 export async function deleteLocation(dbArgs: DatabaseAuthArgs, uuid: string) {
-  const token = await getAuth(dbArgs)
-  return await fetchDb<Location>(DbWriteFunction.DeleteLocation, { ...dbArgs, token }, JSON.stringify({ location_uuid: uuid }))
+  return await fetchDb<Location>(DbWriteFunction.DeleteLocation, await insertTokenIntoAuth(dbArgs), JSON.stringify({ location_uuid: uuid }))
 }
 
 export async function insertLocationsClusterSet(dbArgs: DatabaseAuthArgs, data: InsertLocationsClustersSetParams) {
-  const token = await getAuth(dbArgs)
-  return await fetchDb<Location>(DbWriteFunction.InsertLocationsClustersSet, { ...dbArgs, token }, JSON.stringify(data))
+  return await fetchDb<Location>(DbWriteFunction.InsertLocationsClustersSet, await insertTokenIntoAuth(dbArgs), JSON.stringify(data))
 }
 
 export async function flushClusterTable(dbArgs: DatabaseAuthArgs) {
-  const token = await getAuth(dbArgs)
-  return await fetchDb<Location>(DbWriteFunction.FlushClustersTable, { ...dbArgs, token })
+  return await fetchDb<Location>(DbWriteFunction.FlushClustersTable, await insertTokenIntoAuth(dbArgs))
 }
