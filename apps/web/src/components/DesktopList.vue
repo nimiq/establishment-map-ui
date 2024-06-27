e940c55d2bb3f702c6e0ed8c84d3333f4b90424b
Maxi <maximogarciamtnez@gmail.com>
2024-06-27 18:16:21 +0300

    lint

--- apps/web/src/components/DesktopList.vue
+++ apps/web/src/components/DesktopList.vue
@@ -25,18 +25,22 @@ watch(selectedUuid, (uuid) => {
 </script>
 
 <template>
-  <DynamicScroller v-if="singles.length || clusters.length" ref="scroller" key-field="uuid" :items="singles"
+  <DynamicScroller
+    v-if="singles.length || clusters.length" ref="scroller" key-field="uuid" :items="singles"
     :min-item-size="99" list-tag="ul" item-tag="li" max-w-320 of-auto transition-height duration-300 will-change-height
     :style="{ height: listIsShown ? 'calc(100vh - 10.5rem - var(--dynamic-block, 0) * 88px)' : '0' }"
     item-class="relative of-hidden ring-neutral-100 border-t-1 group/card [&_[data-rings]]:-rotate-90"
-    :data-state="listIsShown ? 'open' : 'closed'">
+    :data-state="listIsShown ? 'open' : 'closed'"
+  >
     <template #default="{ item: location, active }">
       <DynamicScrollerItem :key="location.uuid" :item="location" :active="active">
-        <button w-full px-24 py-20 text-left bg="$bg-1 image-$bg-1 hocus:$bg-2 hocus:image-$bg-2" transition-colors
+        <button
+          w-full px-24 py-20 text-left bg="$bg-1 image-$bg-1 hocus:$bg-2 hocus:image-$bg-2" transition-colors
           :data-inverted="location.isAtm && location.cardStyle.isDark ? 'true' : undefined" :style="{
             '--bg-1': location.isAtm && location.cardStyle.isDark ? location.style.bg[0] : 'rgb(var(--nq-neutral-0))',
             '--bg-2': location.isAtm && location.cardStyle.isDark && location.style.bg[1] ? location.style.bg[1] : 'rgb(var(--nq-neutral-100))',
-          }" @click="useLocations().goToLocation(location.uuid, { open: true })">
+          }" @click="useLocations().goToLocation(location.uuid, { open: true })"
+        >
           <LocationCardBg v-if="location.isAtm" :location="location" :with-gradient="false" translate-y-1 />
           <BasicInfoLocation :location="location" />
         </button>
@@ -49,9 +53,11 @@ watch(selectedUuid, (uuid) => {
       </div>
     </template>
   </DynamicScroller>
-  <div v-else flex="~ col items-center justify-center gap-24" max-w-320 of-hidden px-20 transition-height duration-300
+  <div
+    v-else flex="~ col items-center justify-center gap-24" max-w-320 of-hidden px-20 transition-height duration-300
     will-change-height
-    :style="{ height: listIsShown ? 'calc(100vh - 10.5rem - var(--dynamic-block, 0) * 88px)' : '0' }">
+    :style="{ height: listIsShown ? 'calc(100vh - 10.5rem - var(--dynamic-block, 0) * 88px)' : '0' }"
+  >
     <div i-nimiq:icons-lg-cactus text-80 op-80 />
     <span text="16 center">
       {{ $t('Oops, no businesses around here') }}
